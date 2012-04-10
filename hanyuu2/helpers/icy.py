180e6f26efcf5236f1918a8baf3c48f77090581a
Stacey Ell <stacey.ell@gmail.com>
2012-04-10 15:34:29 -0600

    ICY metadata creation, decoding fixes

--- hanyuu2/helpers/icy.py
+++ hanyuu2/helpers/icy.py
@@ -7,17 +7,15 @@ def _parse_key(input_, pos):
     return retval, pos
 
 def _parse_quoted_string(input_, pos):
+    # apparently ICY quoted strings are not quite quoted strings,
+    # they don't escape their quote, so just scan until ';
     out = ''
-    assert input_[pos] == "'"
+    assert input_[pos] == "'", "at %d in %r" % (pos, input_)
     pos += 1
     while True:
-        if input_[pos] == "'":
+        if input_[pos:pos+2] == "';":
             pos += 1
             break
-        elif input_[pos] == "\\":
-            pos += 1
-            out += input_[pos]
-            pos += 1
         else:
             out += input_[pos]
             pos += 1
@@ -26,7 +24,7 @@ def _parse_quoted_string(input_, pos):
 def _parse_element(input_, pos):
     key, pos = _parse_key(input_, pos)
     val, pos = _parse_quoted_string(input_, pos)
-    assert input_[pos] == ';'
+    assert input_[pos] == ';', "at %d in %r" % (pos, input_)
     pos += 1
     return (key, val), pos
 
@@ -38,4 +36,30 @@ def parse_icy(input_):
         element, pos = _parse_element(input_, pos)
         elements.append(element)
     return elements
+
+def _generate_icy(data):
+    # I am unsure of the specification, so this will remain ugly for now.
+    elements = list()
+    if 'StreamTitle' in data:
+        elements.append("%s='%s';" % ('StreamTitle', data['StreamTitle']))
+    if 'X-Track-Id' in data:
+        elements.append("%s=%s;" % ('X-Track-Id', data['X-Track-Id']))
+    buf = ''.join(elements)
+    buf += "\x00" * (-len(buf) % 16)
+    return buf
+
+def metainfo_injector(meta_info_getter, inject_freq, src_queue):
+    # meta_info_getter should yield empty dictionaries if the status
+    # has not been modified
+    out_queue = gevent.queue.Queue()
+    def gloop():
+        # FIXME: use a ring buffer later
+        buf = ''
+        for read_data in src_queue.get():
+            buf += read_data
+            while inject_freq < len(buf):
+                buf, tmp = buf[0:inject_freq], buf[inject_freq:]
+                out_queue.put(tmp)
+                out_queue.put(_generate_icy(meta_info_getter()))
+        out_queue.put(StopIteration)
 
